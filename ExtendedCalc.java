89fb91068253c5bbe520bc13dc70720f651b571a
Sanjeev K <01fe22bca238@kletech.ac.in>
2024-02-14 15:15:06 +0530

    adding two files for extended branch

---
+++ ExtendedCalc.java
@@ -0,0 +1,13 @@
+public class ExtendedCalc {
+    float modulus(){
+        float operand_1 = 12;
+        float operand_2 = 24;
+        return (operand_1 % operand_2);
+
+    }
+    float area_of_triangle(){
+        float operand_1 = 10;
+        float operand_2 = 5;
+        return (1/2*operand_1 *operand_2);
+    }
+}
